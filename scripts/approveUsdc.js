7f19d61e219c225ac04178d5b8c98e8d82653573
Patrick Hughes <patrick.hughes@coinbase.com>
2024-06-18 21:02:58 -0500

    remove comments

--- scripts/approveUsdc.js
+++ scripts/approveUsdc.js
@@ -1,10 +1,6 @@
 import 'dotenv/config';
 import { encodeFunctionData, getContract, parseUnits } from 'viem';
-import {
-  aeroUniversalRouterAbi,
-  walletClient,
-  baseUsdcAbi, // This should be the ABI of the implementation contract
-} from '../utils.js';
+import { aeroUniversalRouterAbi, walletClient, baseUsdcAbi } from '../utils.js';
 
 async function main() {
   const aerodromeUniversalRouter = getContract({
@@ -14,51 +10,39 @@ async function main() {
   });
 
   const baseUsdc = getContract({
-    address: process.env.USDC_BASE, // Proxy contract address
-    abi: baseUsdcAbi, // Implementation contract ABI
+    address: process.env.USDC_BASE,
+    abi: baseUsdcAbi,
     client: walletClient,
   });
 
-  console.log('USDC Proxy Address: ', baseUsdc.address);
-  console.log('Aerodrome Router Address: ', aerodromeUniversalRouter.address);
-
   const spender = aerodromeUniversalRouter.address;
-  const value = parseUnits('1', 6); // USDC uses 6 decimals
+  const value = parseUnits('1', 6);
 
   try {
     const [account] = await walletClient.getAddresses();
     console.log('Using account:', account);
-
-    // const { request: usdcApprovalRequest } =
-    //   await publicClient.simulateContract({
-    //     address: baseUsdc.address, // Use the proxy address
-    //     account: account,
-    //     abi: baseUsdcAbi, // Use the implementation ABI
-    //     functionName: 'approve',
-    //     args: [spender, value],
-    //   });
 
     const data = encodeFunctionData({
       abi: baseUsdcAbi,
       functionName: 'approve',
       args: [spender, value],
     });
-    console.log('data: ', data);
+    console.log('Encoded "approve" function: ', data);
 
     const request = await walletClient.prepareTransactionRequest({
       to: baseUsdc.address,
       data: data,
     });
     console.log('Approval request:', request);
+
     const signature = await walletClient.signTransaction(request);
-    console.log('sig: ', signature);
+    console.log('Signed transaction: ', signature);
 
     const hash = await walletClient.sendRawTransaction({
       serializedTransaction: signature,
     });
-    console.log('tx sent: ', hash);
-    //const tx = await walletClient.writeContract(usdcApprovalRequest);
-    //console.log('Approval transaction sent successfully:', tx);
+
+    console.log('Approval transaction sent successfully:', hash);
   } catch (error) {
     console.error('Error during contract interaction:', error);
   }
