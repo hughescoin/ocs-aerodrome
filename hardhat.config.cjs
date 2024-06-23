1cb449d5e34caab3bf4f025b3d34ae2b7cf75926
Patrick Hughes <patrick.hughes@coinbase.com>
2024-06-23 09:07:05 -0500

    with maxFees

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -3,5 +3,22 @@ require('@nomicfoundation/hardhat-viem');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.24',
+  optimizer: {
+    enabled: true,
+    runs: 1_000_000,
+  },
+  ignition: {
+    maxFeePerGasLimit: 50_000_000_000n, // 50 gwei
+    maxPriorityFeePerGas: 2_000_000_000n, // 2 gwei
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.24',
+      },
+      {
+        version: '0.7.6',
+      },
+    ],
+  },
 };
